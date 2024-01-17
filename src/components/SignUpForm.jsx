ecf41bbe74d926358cfa795613ded13c6849af3c
desh01prajakta <prajudeshmukh11@gmail.com>
2024-01-16 22:04:56 -0600

    complete assignment

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({ setToken }) {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
- /* const [userNameError, setUserNameError] = useState('');
+  const [userNameError, setUserNameError] = useState('');
+  
+  const handleUsernameChange = (e) => {
+    const value = e.target.value;
+    setUserName(value);
 
   if (value.length !== 8) {
     setUserNameError('Username must be eight characters long.');
   } else {
     setUserNameError('');
-  }*/
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Additional form submission logic can go here
+
+    // If the form is valid, proceed with submission
+    if (userName.length === 8) {
+      console.log('Form submitted:', userName);
+    } else {
+      console.log('Form is invalid. Please correct the errors.');
+    }
+}
+  }
   async function handleSubmit(event) {
     event.preventDefault();
     try {
@@ -24,13 +41,15 @@ export default function SignUpForm() {
       const result = await response.json();
       setToken(result.token);
       console.log(result);
+      setUserName("");
+      setPassword("");
     } catch (error) {
       setError(error.message);
     }
   }
   return (
     <section>
-      <h2 id="sign" >Sign Up!</h2>
+      <h2 id="sign">Sign Up!</h2>
       {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
@@ -49,7 +68,6 @@ export default function SignUpForm() {
         </label>
         <button id="submit">Submit</button>
       </form>
-
     </section>
   );
 }
